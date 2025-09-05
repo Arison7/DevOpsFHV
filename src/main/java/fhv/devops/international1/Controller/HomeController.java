6c250a65f608e9751fa4a4d617f467497cdda827
Nand Beeckx <nand.beeckx@student.kdg.be>
2025-09-05 10:08:23 +0200

    Added simple Spring boot app with home page

--- src/main/java/fhv/devops/international1/Controller/HomeController.java
+++ src/main/java/fhv/devops/international1/Controller/HomeController.java
@@ -1,4 +1,11 @@
 package fhv.devops.international1.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-public class Homecontroller {
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String HomeView(){
+        return "home";
+    }
 }
