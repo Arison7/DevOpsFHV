cd6c0c2ca35229d2cb1d4ac5a8a5312f68c900ec
Nand Beeckx <nand.beeckx@students.fhv.at>
2025-11-06 15:19:18 +0100

    Added J unit api test

---
+++ src/test/java/fhv/devops/international1/Test/ApiTest.java
@@ -0,0 +1,23 @@
+package fhv.devops.international1.Test;
+
+import fhv.devops.international1.Controller.TestRestController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(TestRestController.class)
+public class ApiTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetEndpoint() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/test"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("success"));
+    }
+}
